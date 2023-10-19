ed6b5cc4767a5f1fff799d28fcf1a53847265039
zeichenreihe <67289119+zeichenreihe@users.noreply.github.com>
2023-10-20 01:47:50 +0200

    Simplify version graph code, and update the storage of diffs and mappings

--- src/reader/tiny_v2.rs
+++ src/reader/tiny_v2.rs
@@ -1,163 +1,185 @@
+use std::collections::HashMap;
 use std::fmt::Debug;
 use std::fs::File;
 use std::path::Path;
 use anyhow::{anyhow, Context, Result};
 use crate::reader::{AddMember, parse, ParseEntry, SetDoc, try_read, try_read_optional};
 
-pub fn read(path: impl AsRef<Path> + Debug) -> Result<Mapping> {
-	parse::<Mapping, ClassMapping, FieldMapping, MethodMapping, ParameterMapping, JavadocMapping>(File::open(&path)?)
+pub fn read(path: impl AsRef<Path> + Debug) -> Result<Mappings> {
+	parse::<Mappings, ClassMapping, FieldMapping, MethodMapping, ParameterMapping, JavadocMapping>(File::open(&path)?)
 		.with_context(|| anyhow!("Failed to read file {path:?}"))
 }
 
-#[derive(Debug)]
-pub struct Mapping {
-	pub classes: Vec<ClassMapping>,
+#[derive(Debug, Clone)]
+pub struct Mappings {
 	pub src: String,
 	pub dst: String,
+	/// Maps from `src` to the mapping
+	pub classes: HashMap<String, ClassMapping>,
 }
 
-impl ParseEntry for Mapping {
+impl ParseEntry for Mappings {
 	fn from_iter<'a>(iter: &mut impl Iterator<Item=&'a str>) -> Result<Self> {
-		Ok(Mapping {
+		Ok(Mappings {
 			src: iter.next()
 				.ok_or_else(|| anyhow!("expected two namespaces"))?
 				.to_owned(),
 			dst: iter.next()
 				.ok_or_else(|| anyhow!("expected two namespaces"))?
 				.to_owned(),
-			classes: Vec::new(),
+			classes: HashMap::new(),
 		})
 	}
 }
-impl AddMember<ClassMapping> for Mapping {
+impl AddMember<ClassMapping> for Mappings {
 	fn add_member(&mut self, member: ClassMapping) {
-		self.classes.push(member)
+		self.classes.insert(member.src.clone(), member);
 	}
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug, Clone)]
 pub struct ClassMapping {
 	pub src: String,
 	pub dst: String,
 
-	pub jav: JavadocMapping,
+	pub jav: Option<JavadocMapping>,
 
-	pub fields: Vec<FieldMapping>,
-	pub methods: Vec<MethodMapping>,
+	pub fields: HashMap<(String, String), FieldMapping>,
+	pub methods: HashMap<(String, String), MethodMapping>,
 }
 
+impl ClassMapping {
+	pub fn new(src: String, dst: String) -> ClassMapping {
+		ClassMapping {
+			src, dst,
+			jav: None,
+			fields: HashMap::new(),
+			methods: HashMap::new(),
+		}
+	}
+}
 impl ParseEntry for ClassMapping {
 	fn from_iter<'a>(iter: &mut impl Iterator<Item=&'a str>) -> Result<Self> {
-		Ok(ClassMapping {
-			src: try_read(iter)?,
-			dst: try_read(iter)?,
-			..Default::default()
-		})
+		Ok(ClassMapping::new(try_read(iter)?, try_read(iter)?))
 	}
 }
 impl SetDoc<JavadocMapping> for ClassMapping {
 	fn set_doc(&mut self, doc: JavadocMapping) {
-		self.jav = doc;
+		self.jav = Some(doc);
 	}
 }
 impl AddMember<FieldMapping> for ClassMapping {
 	fn add_member(&mut self, member: FieldMapping) {
-		self.fields.push(member);
+		self.fields.insert((member.desc.clone(), member.src.clone()), member);
 	}
 }
 impl AddMember<MethodMapping> for ClassMapping {
 	fn add_member(&mut self, member: MethodMapping) {
-		self.methods.push(member);
+		self.methods.insert((member.desc.clone(), member.src.clone()), member);
 	}
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug, Clone)]
 pub struct FieldMapping {
 	pub desc: String,
 
 	pub src: String,
 	pub dst: String,
 
-	pub jav: JavadocMapping,
+	pub jav: Option<JavadocMapping>,
 }
 
+impl FieldMapping {
+	pub fn new(desc: String, src: String, dst: String) -> FieldMapping {
+		FieldMapping {
+			desc, src, dst,
+			jav: None,
+		}
+	}
+}
 impl ParseEntry for FieldMapping {
 	fn from_iter<'a>(iter: &mut impl Iterator<Item=&'a str>) -> Result<Self> {
-		Ok(FieldMapping {
-			desc: try_read(iter)?,
-			src: try_read(iter)?,
-			dst: try_read(iter)?,
-			..Default::default()
-		})
+		Ok(FieldMapping::new(try_read(iter)?, try_read(iter)?, try_read(iter)?))
 	}
 }
 impl SetDoc<JavadocMapping> for FieldMapping {
 	fn set_doc(&mut self, doc: JavadocMapping) {
-		self.jav = doc;
+		self.jav = Some(doc);
 	}
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug, Clone)]
 pub struct MethodMapping {
 	pub desc: String,
 
 	pub src: String,
 	pub dst: String,
 
-	pub jav: JavadocMapping,
+	pub jav: Option<JavadocMapping>,
 
-	pub parameters: Vec<ParameterMapping>,
+	pub parameters: HashMap<usize, ParameterMapping>,
 }
 
+impl MethodMapping {
+	pub fn new(desc: String, src: String, dst: String) -> MethodMapping {
+		MethodMapping {
+			desc, src, dst,
+			jav: None,
+			parameters: HashMap::new(),
+		}
+	}
+}
 impl ParseEntry for MethodMapping {
 	fn from_iter<'a>(iter: &mut impl Iterator<Item=&'a str>) -> Result<Self> {
-		Ok(MethodMapping {
-			desc: try_read(iter)?,
-			src: try_read(iter)?,
-			dst: try_read(iter)?,
-			..Default::default()
-		})
+		Ok(MethodMapping::new(try_read(iter)?, try_read(iter)?, try_read(iter)?))
 	}
 }
 impl SetDoc<JavadocMapping> for MethodMapping {
 	fn set_doc(&mut self, doc: JavadocMapping) {
-		self.jav = doc;
+		self.jav = Some(doc);
 	}
 }
 impl AddMember<ParameterMapping> for MethodMapping {
 	fn add_member(&mut self, member: ParameterMapping) {
-		self.parameters.push(member);
+		self.parameters.insert(member.index, member);
 	}
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug, Clone)]
 pub struct ParameterMapping {
 	pub src: String,
 	pub dst: String,
 
-	pub jav: JavadocMapping,
+	pub jav: Option<JavadocMapping>,
 
 	pub index: usize,
 }
 
+impl ParameterMapping {
+	pub fn new(index: usize, src: String, dst: String) -> ParameterMapping {
+		ParameterMapping {
+			index, src, dst,
+			jav: None,
+		}
+	}
+}
 impl ParseEntry for ParameterMapping {
 	fn from_iter<'a>(iter: &mut impl Iterator<Item=&'a str>) -> Result<Self> {
-		Ok(ParameterMapping {
-			index: try_read(iter)?.parse()
+		Ok(ParameterMapping::new(
+			try_read(iter)?.parse()
 				.with_context(|| anyhow!("illegal parameter index"))?,
-			src: try_read_optional(iter)?.unwrap_or(String::new()), // TODO: ask space what this means, change to `try_read(&mut iter)` to see it fail
-			dst: try_read(iter)?,
-			..Default::default()
-		})
+			try_read_optional(iter)?.unwrap_or(String::new()), // TODO: ask space what this means, change to `try_read(&mut iter)` to see it fail
+			try_read(iter)?
+		))
 	}
 }
 impl SetDoc<JavadocMapping> for ParameterMapping {
 	fn set_doc(&mut self, doc: JavadocMapping) {
-		self.jav = doc;
+		self.jav = Some(doc);
 	}
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug, Clone)]
 pub struct JavadocMapping {
 	pub jav: String,
 }
@@ -166,7 +188,6 @@ impl ParseEntry for JavadocMapping {
 	fn from_iter<'a>(iter: &mut impl Iterator<Item=&'a str>) -> Result<Self> {
 		Ok(JavadocMapping {
 			jav: try_read(iter)?,
-			..Default::default()
 		})
 	}
 }
--- src/reader/tiny_v2_diff.rs
+++ src/reader/tiny_v2_diff.rs
@@ -1,43 +1,125 @@
+use std::collections::hash_map::Entry;
+use std::collections::HashMap;
 use std::fmt::Debug;
 use std::fs::File;
+use std::hash::Hash;
 use std::path::Path;
 use anyhow::{anyhow, bail, Context, Result};
 use crate::reader::{AddMember, parse, ParseEntry, SetDoc, try_read, try_read_optional};
+use crate::reader::tiny_v2::{ClassMapping, FieldMapping, JavadocMapping, Mappings, MethodMapping, ParameterMapping};
 
-pub fn read(path: impl AsRef<Path> + Debug) -> Result<Diff> {
-	parse::<Diff, ClassDiff, FieldDiff, MethodDiff, ParameterDiff, JavadocDiff>(File::open(&path)?)
+pub fn read(path: impl AsRef<Path> + Debug) -> Result<Diffs> {
+	parse::<Diffs, ClassDiff, FieldDiff, MethodDiff, ParameterDiff, JavadocDiff>(File::open(&path)?)
 		.with_context(|| anyhow!("Failed to read file {path:?}"))
 }
 
-#[derive(Debug)]
-pub struct Diff {
+pub trait ApplyDiff<T> {
+	fn apply_to(&self, target: T) -> Result<()>;
+}
+trait GetKey<V> {
+	fn get_key(&self) -> V;
+}
+
+impl<'a, T, U> ApplyDiff<&'a mut Option<U>> for Option<T>
+	where
+		T: ApplyDiff<&'a mut Option<U>>,
+{
+	fn apply_to(&self, target: &'a mut Option<U>) -> Result<()> {
+		match self {
+			Some(x) => x.apply_to(target)?,
+			None => *target = None,
+		}
+		Ok(())
+	}
+}
+
+impl<'a, T, U, V> ApplyDiff<Entry<'a, U, V>> for Option<T>
+where
+	T: ApplyDiff<Entry<'a, U, V>>,
+{
+	fn apply_to(&self, entry: Entry<'a, U, V>) -> Result<()> {
+		match self {
+			Some(x) => x.apply_to(entry)?,
+			None => {
+				if let Entry::Occupied(entry) = entry {
+					entry.remove();
+				}
+			},
+		}
+		Ok(())
+	}
+}
+
+impl<T, U, V> ApplyDiff<&mut HashMap<V, U>> for Vec<T>
+where
+	T: for<'a> ApplyDiff<Entry<'a, V, U>> + GetKey<V>,
+	U: Clone,
+	V: Eq + Hash,
+{
+	fn apply_to(&self, target: &mut HashMap<V, U>) -> Result<()> {
+		for diff in self {
+			diff.apply_to(target.entry(diff.get_key()))?;
+		}
+
+		Ok(())
+	}
+}
+
+#[derive(Debug, Clone)]
+pub enum Operation<'a> {
+	None,
+	Change(&'a String, &'a String),
+	Add(&'a String),
+	Remove(&'a String),
+}
+
+impl Operation<'_> {
+	pub fn of<'a>(a: &'a Option<String>, b: &'a Option<String>) -> Operation<'a> {
+		match (a, b) {
+			(None, Some(b)) => Operation::Add(b),
+			(Some(a), None) => Operation::Remove(a),
+			(Some(a), Some(b)) if a != b => Operation::Change(a, b),
+			_ => Operation::None,
+		}
+	}
+}
+
+#[derive(Debug, Clone)]
+pub struct Diffs {
 	pub classes: Vec<ClassDiff>,
 }
 
-impl ParseEntry for Diff {
+impl ParseEntry for Diffs {
 	fn from_iter<'a>(iter: &mut impl Iterator<Item=&'a str>) -> Result<Self> {
 		if iter.next().is_some() {
 			bail!("expected empty namespaces for a diff");
 		}
 
-		Ok(Diff {
+		Ok(Diffs {
 			classes: Vec::new(),
 		})
 	}
 }
-impl AddMember<ClassDiff> for Diff {
+impl AddMember<ClassDiff> for Diffs {
 	fn add_member(&mut self, member: ClassDiff) {
 		self.classes.push(member)
 	}
 }
+impl ApplyDiff<&mut Mappings> for Diffs {
+	fn apply_to(&self, target: &mut Mappings) -> Result<()> {
+		self.classes.apply_to(&mut target.classes)
+			.with_context(|| anyhow!("Failed on applying diff to mapping `{}` `{}`", target.src, target.dst))?;
+		Ok(())
+	}
+}
 
-#[derive(Debug, Default)]
+#[derive(Debug, Clone)]
 pub struct ClassDiff {
 	pub src: String,
 	pub dst_a: Option<String>,
 	pub dst_b: Option<String>,
 
-	pub jav: JavadocDiff,
+	pub jav: Option<JavadocDiff>,
 
 	pub fields: Vec<FieldDiff>,
 	pub methods: Vec<MethodDiff>,
@@ -49,13 +131,15 @@ impl ParseEntry for ClassDiff {
 			src: try_read(iter)?,
 			dst_a: try_read_optional(iter)?,
 			dst_b: try_read_optional(iter)?,
-			..Self::default()
+			jav: None,
+			fields: Vec::new(),
+			methods: Vec::new(),
 		})
 	}
 }
 impl SetDoc<JavadocDiff> for ClassDiff {
 	fn set_doc(&mut self, doc: JavadocDiff) {
-		self.jav = doc;
+		self.jav = Some(doc);
 	}
 }
 impl AddMember<FieldDiff> for ClassDiff {
@@ -68,8 +152,69 @@ impl AddMember<MethodDiff> for ClassDiff {
 		self.methods.push(member)
 	}
 }
+impl<T: Debug> ApplyDiff<Entry<'_, T, ClassMapping>> for ClassDiff {
+	fn apply_to(&self, entry: Entry<'_, T, ClassMapping>) -> Result<()> {
+		match entry {
+			Entry::Occupied(mut entry) => {
+				match Operation::of(&self.dst_a, &self.dst_b) {
+					Operation::None => {
+						self.jav.apply_to(&mut entry.get_mut().jav)?;
+						self.fields.apply_to(&mut entry.get_mut().fields)?;
+						self.methods.apply_to(&mut entry.get_mut().methods)?;
+					},
+					Operation::Change(dst_a, dst_b) => {
+						if &entry.get().dst != dst_a {
+							bail!("Cannot change: got {:?} but expected {dst_a}, to map to {dst_b}", entry.get())
+						}
+						entry.get_mut().dst = dst_b.to_owned();
 
-#[derive(Debug, Default)]
+						self.jav.apply_to(&mut entry.get_mut().jav)?;
+						self.fields.apply_to(&mut entry.get_mut().fields)?;
+						self.methods.apply_to(&mut entry.get_mut().methods)?;
+					},
+					Operation::Add(dst_b) => {
+						bail!("Cannot add item {dst_b}: already existing: {entry:?}")
+					},
+					Operation::Remove(dst_a) => {
+						if &entry.get().dst != dst_a {
+							bail!("Cannot remove: got {:?} but expected {dst_a}", entry.get());
+						}
+						entry.remove();
+					},
+				}
+			},
+			Entry::Vacant(entry) => {
+				match Operation::of(&self.dst_a, &self.dst_b) {
+					Operation::None => {},
+					Operation::Change(dst_a, dst_b) => {
+						bail!("Cannot change item {dst_a} to {dst_b}: no item given")
+					},
+					Operation::Add(dst_b) => {
+						let mut v = ClassMapping::new(self.src.clone(), dst_b.clone());
+
+						self.jav.apply_to(&mut v.jav)?;
+						self.fields.apply_to(&mut v.fields)?;
+						self.methods.apply_to(&mut v.methods)?;
+
+						entry.insert(v);
+					},
+					Operation::Remove(dst_a) => {
+						bail!("Cannot remove item {dst_a}: no item given")
+					},
+				}
+			},
+		}
+
+		Ok(())
+	}
+}
+impl GetKey<String> for ClassDiff {
+	fn get_key(&self) -> String {
+		self.src.clone()
+	}
+}
+
+#[derive(Debug, Clone)]
 pub struct FieldDiff {
 	pub desc: String,
 
@@ -77,7 +222,7 @@ pub struct FieldDiff {
 	pub dst_a: Option<String>,
 	pub dst_b: Option<String>,
 
-	pub jav: JavadocDiff,
+	pub jav: Option<JavadocDiff>,
 }
 
 impl ParseEntry for FieldDiff {
@@ -87,17 +232,69 @@ impl ParseEntry for FieldDiff {
 			src: try_read(iter)?,
 			dst_a: try_read_optional(iter)?,
 			dst_b: try_read_optional(iter)?,
-			..Default::default()
+			jav: None,
 		})
 	}
 }
 impl SetDoc<JavadocDiff> for FieldDiff {
 	fn set_doc(&mut self, doc: JavadocDiff) {
-		self.jav = doc;
+		self.jav = Some(doc);
+	}
+}
+impl<T: Debug> ApplyDiff<Entry<'_, T, FieldMapping>> for FieldDiff {
+	fn apply_to(&self, entry: Entry<'_, T, FieldMapping>) -> Result<()> {
+		match entry {
+			Entry::Occupied(mut entry) => {
+				match Operation::of(&self.dst_a, &self.dst_b) {
+					Operation::None => {
+						self.jav.apply_to(&mut entry.get_mut().jav)?;
+					},
+					Operation::Change(dst_a, dst_b) => {
+						if &entry.get().dst != dst_a {
+							bail!("Cannot change: got {:?} but expected {dst_a}, to map to {dst_b}", entry.get())
+						}
+						entry.get_mut().dst = dst_b.to_owned();
+						self.jav.apply_to(&mut entry.get_mut().jav)?;
+					},
+					Operation::Add(dst_b) => {
+						bail!("Cannot add item {dst_b}: already existing: {entry:?}")
+					},
+					Operation::Remove(dst_a) => {
+						if &entry.get().dst != dst_a {
+							bail!("Cannot remove: got {:?} but expected {dst_a}", entry.get());
+						}
+						entry.remove();
+					},
+				}
+			},
+			Entry::Vacant(entry) => {
+				match Operation::of(&self.dst_a, &self.dst_b) {
+					Operation::None => {},
+					Operation::Change(dst_a, dst_b) => {
+						bail!("Cannot change item {dst_a} to {dst_b}: no item given")
+					},
+					Operation::Add(dst_b) => {
+						let mut f = FieldMapping::new(self.desc.clone(), self.src.clone(), dst_b.clone());
+						self.jav.apply_to(&mut f.jav)?;
+						entry.insert(f);
+					},
+					Operation::Remove(dst_a) => {
+						bail!("Cannot remove item {dst_a}: no item given")
+					},
+				}
+			},
+		}
+
+		Ok(())
+	}
+}
+impl GetKey<(String, String)> for FieldDiff {
+	fn get_key(&self) -> (String, String) {
+		(self.desc.clone(), self.src.clone())
 	}
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug, Clone)]
 pub struct MethodDiff {
 	pub desc: String,
 
@@ -105,7 +302,7 @@ pub struct MethodDiff {
 	pub dst_a: Option<String>,
 	pub dst_b: Option<String>,
 
-	pub jav: JavadocDiff,
+	pub jav: Option<JavadocDiff>,
 
 	pub parameters: Vec<ParameterDiff>,
 }
@@ -117,13 +314,14 @@ impl ParseEntry for MethodDiff {
 			src: try_read(iter)?,
 			dst_a: try_read_optional(iter)?,
 			dst_b: try_read_optional(iter)?,
-			..Default::default()
+			jav: None,
+			parameters: Vec::new(),
 		})
 	}
 }
 impl SetDoc<JavadocDiff> for MethodDiff {
 	fn set_doc(&mut self, doc: JavadocDiff) {
-		self.jav = doc;
+		self.jav = Some(doc);
 	}
 }
 impl AddMember<ParameterDiff> for MethodDiff {
@@ -131,37 +329,145 @@ impl AddMember<ParameterDiff> for MethodDiff {
 		self.parameters.push(member)
 	}
 }
+impl<T: Debug> ApplyDiff<Entry<'_, T, MethodMapping>> for MethodDiff {
+	fn apply_to(&self, entry: Entry<'_, T, MethodMapping>) -> Result<()> {
+		match entry {
+			Entry::Occupied(mut entry) => {
+				match Operation::of(&self.dst_a, &self.dst_b) {
+					Operation::None => {
+						self.jav.apply_to(&mut entry.get_mut().jav)?;
+						self.parameters.apply_to(&mut entry.get_mut().parameters)?;
+					},
+					Operation::Change(dst_a, dst_b) => {
+						if &entry.get().dst != dst_a {
+							bail!("Cannot change: got {:?} but expected {dst_a}, to map to {dst_b}", entry.get())
+						}
+						entry.get_mut().dst = dst_b.to_owned();
+						self.jav.apply_to(&mut entry.get_mut().jav)?;
+						self.parameters.apply_to(&mut entry.get_mut().parameters)?;
+					},
+					Operation::Add(dst_b) => {
+						bail!("Cannot add item {dst_b}: already existing: {entry:?}")
+					},
+					Operation::Remove(dst_a) => {
+						if &entry.get().dst != dst_a {
+							bail!("Cannot remove: got {:?} but expected {dst_a}", entry.get());
+						}
+						entry.remove();
+					},
+				}
+			},
+			Entry::Vacant(entry) => {
+				match Operation::of(&self.dst_a, &self.dst_b) {
+					Operation::None => {},
+					Operation::Change(dst_a, dst_b) => {
+						bail!("Cannot change item {dst_a} to {dst_b}: no item given")
+					},
+					Operation::Add(dst_b) => {
+						let mut v = MethodMapping::new(self.desc.clone(), self.src.clone(), dst_b.clone());
 
-#[derive(Debug, Default)]
+						self.jav.apply_to(&mut v.jav)?;
+						self.parameters.apply_to(&mut v.parameters)?;
+
+						entry.insert(v);
+					},
+					Operation::Remove(dst_a) => {
+						bail!("Cannot remove item {dst_a}: no item given")
+					},
+				}
+			},
+		}
+
+		Ok(())
+	}
+}
+impl GetKey<(String, String)> for MethodDiff {
+	fn get_key(&self) -> (String, String) {
+		(self.desc.clone(), self.src.clone())
+	}
+}
+
+#[derive(Debug, Clone)]
 pub struct ParameterDiff {
 	pub src: String,
 	pub dst_a: Option<String>,
 	pub dst_b: Option<String>,
 
-	pub jav: JavadocDiff,
+	pub jav: Option<JavadocDiff>,
 
 	pub index: usize,
 }
 
 impl ParseEntry for ParameterDiff {
-	fn from_iter<'a>(iter: &mut impl Iterator<Item=&'a str>) -> Result<Self> where Self: Sized {
+	fn from_iter<'a>(iter: &mut impl Iterator<Item=&'a str>) -> Result<Self> {
+		let index = try_read(iter)?.parse()
+			.with_context(|| anyhow!("illegal parameter index"))?;
 		Ok(ParameterDiff {
-			index: try_read(iter)?.parse()
-				.with_context(|| anyhow!("illegal parameter index"))?,
+			index,
 			src: try_read_optional(iter)?.unwrap_or(String::new()), // TODO: ask space what this means, change to `try_read(&mut iter)` to see it fail
 			dst_a: try_read_optional(iter)?,
 			dst_b: try_read_optional(iter)?,
-			..Default::default()
+			jav: None,
 		})
 	}
 }
 impl SetDoc<JavadocDiff> for ParameterDiff {
 	fn set_doc(&mut self, doc: JavadocDiff) {
-		self.jav = doc;
+		self.jav = Some(doc);
+	}
+}
+impl<T: Debug> ApplyDiff<Entry<'_, T, ParameterMapping>> for ParameterDiff {
+	fn apply_to(&self, entry: Entry<'_, T, ParameterMapping>) -> Result<()> {
+		match entry {
+			Entry::Occupied(mut entry) => {
+				match Operation::of(&self.dst_a, &self.dst_b) {
+					Operation::None => self.jav.apply_to(&mut entry.get_mut().jav)?,
+					Operation::Change(dst_a, dst_b) => {
+						if &entry.get().dst != dst_a {
+							bail!("Cannot change: got {:?} but expected {dst_a}, to map to {dst_b}", entry.get())
+						}
+						entry.get_mut().dst = dst_b.to_owned();
+						self.jav.apply_to(&mut entry.get_mut().jav)?;
+					},
+					Operation::Add(dst_b) => {
+						bail!("Cannot add item {dst_b}: already existing: {entry:?}")
+					},
+					Operation::Remove(dst_a) => {
+						if &entry.get().dst != dst_a {
+							bail!("Cannot remove: got {:?} but expected {dst_a}", entry.get());
+						}
+						entry.remove();
+					},
+				}
+			},
+			Entry::Vacant(entry) => {
+				match Operation::of(&self.dst_a, &self.dst_b) {
+					Operation::None => {},
+					Operation::Change(dst_a, dst_b) => {
+						bail!("Cannot change item {dst_a} to {dst_b}: no item given")
+					},
+					Operation::Add(dst_b) => {
+						let mut v = ParameterMapping::new(self.index, self.src.clone(), dst_b.clone());
+						self.jav.apply_to(&mut v.jav)?;
+						entry.insert(v);
+					},
+					Operation::Remove(dst_a) => {
+						bail!("Cannot remove item {dst_a}: no item given")
+					},
+				}
+			},
+		}
+
+		Ok(())
+	}
+}
+impl GetKey<usize> for ParameterDiff {
+	fn get_key(&self) -> usize {
+		self.index
 	}
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug, Clone)]
 pub struct JavadocDiff {
 	pub jav_a: Option<String>,
 	pub jav_b: Option<String>,
@@ -173,5 +479,44 @@ impl ParseEntry for JavadocDiff {
 			jav_a: try_read_optional(iter)?,
 			jav_b: try_read_optional(iter)?,
 		})
+	}
+}
+impl ApplyDiff<&mut Option<JavadocMapping>> for JavadocDiff {
+	fn apply_to(&self, target: &mut Option<JavadocMapping>) -> Result<()> {
+		match Operation::of(&self.jav_a, &self.jav_b) {
+			Operation::None => {},
+			Operation::Change(dst_a, dst_b) => {
+				if let Some(target) = target {
+					if &target.jav != dst_a {
+						bail!("Cannot change: got {:?} but expected {dst_a}, to map to {dst_b}", target)
+					}
+					target.jav = dst_b.to_owned()
+				} else {
+					bail!("Cannot change item: no item given")
+				}
+			},
+			Operation::Add(dst_b) => {
+				if let Some(target) = target {
+					bail!("Cannot add item {dst_b}: already existing: {target:?}")
+				} else {
+					*target = Some(JavadocMapping {
+						jav: dst_b.to_owned(),
+					})
+				}
+			},
+			Operation::Remove(dst_a) => {
+				if target.is_some() {
+					// TODO:
+					//if &target.get().dst != dst_a {
+					//	bail!("Cannot remove: got {:?} but expected {dst_a}", entry.get());
+					//}
+					*target = None;
+				} else {
+					bail!("Cannot remove item {dst_a}: no item given")
+				}
+			},
+		}
+
+		Ok(())
 	}
 }
