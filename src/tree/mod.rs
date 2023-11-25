f6139c406ffea43f41da4e4758b427682ce68a2b
zeichenreihe <67289119+zeichenreihe@users.noreply.github.com>
2023-11-26 00:52:31 +0100

    Implement merging
    Note:
    - We also refactored name storage, since this is needed for easier merging

--- src/tree/mod.rs
+++ src/tree/mod.rs
@@ -1,5 +1,5 @@
+use std::fmt::Debug;
 use anyhow::Result;
-use std::fmt::{Debug};
 use std::hash::Hash;
 use anyhow::bail;
 use indexmap::IndexMap;
@@ -13,6 +13,75 @@ mod action;
 /// When this exists, the namespace it's from has the namespace stored in `.0`, and reading will not panic.
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub(crate) struct Namespace<const N: usize>(usize);
+
+mod names {
+	use std::fmt::{Debug, Formatter};
+	use std::ops::Index;
+	use anyhow::bail;
+	use crate::tree::Namespace;
+
+	/// A struct storing names for namespaces.
+	#[derive(Clone, PartialEq, PartialOrd, Eq, Ord)]
+	pub(crate) struct Names<const N: usize> {
+		/// Conditions to hold true:
+		/// - `index = 0` must always be `Some`
+		names: [Option<String>; N],
+	}
+
+	impl<const N: usize> Names<N> {
+		pub(crate) fn src(&self) -> &String {
+			// SAFETY: guaranteed from the conditions of the struct
+			self.names[0].as_ref().unwrap()
+		}
+
+		pub(crate) fn get(&self, namespace: Namespace<N>) -> Option<&String> {
+			self.names[namespace.0].as_ref()
+		}
+
+		pub(crate) fn names(&self) -> impl Iterator<Item=Option<&String>> {
+			self.names.iter().map(|x| x.as_ref())
+		}
+	}
+
+	impl<const N: usize> Debug for Names<N> {
+		fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+			f.debug_list()
+				.entry(&self.names)
+				.finish()
+		}
+	}
+
+	impl<const N: usize> From<[String; N]> for Names<N> {
+		fn from(value: [String; N]) -> Self {
+			Names {
+				names: value.map(|x| Some(x)),
+			}
+		}
+	}
+
+	impl<const N: usize> TryFrom<[Option<String>; N]> for Names<N> {
+		type Error = anyhow::Error;
+		fn try_from(value: [Option<String>; N]) -> Result<Self, Self::Error> {
+			if value.get(0).is_some_and(|x| x.is_none()) {
+				bail!("Cannot have empty name for namespace zero: {value:?}");
+			}
+			Ok(Names { names: value })
+		}
+	}
+
+	impl<const N: usize> From<Names<N>> for [Option<String>; N] {
+		fn from(value: Names<N>) -> Self {
+			value.names
+		}
+	}
+
+	impl<'a, const N: usize> From<&'a Names<N>> for &'a [Option<String>; N] {
+		fn from(value: &'a Names<N>) -> Self {
+			&value.names
+		}
+	}
+}
+pub(crate) use names::Names;
 
 pub(crate) trait NodeData<T> {
 	fn node_data(&self) -> &T;
