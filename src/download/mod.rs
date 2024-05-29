65b821dd0a0d1c31e9aeae2033d7019a80e22e4d
zeichenreihe <67289119+zeichenreihe@users.noreply.github.com>
2024-05-29 03:39:57 +0200

    Update downloading, and update main.rs
    This includes the new maven metadata xml parsing, as well as putting the build as one large function and not many small ones.
    
    Mappings download code checks for the namespaces now.
    
    Also lowercases more error messages.

--- src/download/mod.rs
+++ src/download/mod.rs
@@ -1,17 +1,23 @@
+use std::fs;
+use std::fs::File;
 use std::io::Cursor;
+use std::path::Path;
 use anyhow::{anyhow, bail, Context, Result};
+use bytes::Buf;
 use reqwest::{Client, Response};
 use zip::ZipArchive;
 use crate::download::version_details::VersionDetails;
 use crate::download::version_manifest::VersionManifest;
 use crate::download::versions_manifest::VersionsManifest;
 use crate::{Environment, Jar};
+use crate::download::maven_metadata::MavenMetadata;
 use crate::tree::mappings::Mappings;
-use crate::version_graph::Version;
+use crate::Version;
 
 pub(crate) mod versions_manifest;
 pub(crate) mod version_manifest;
-pub(crate)mod version_details;
+pub(crate) mod version_details;
+pub(crate) mod maven_metadata;
 
 #[derive(Debug)]
 pub(crate) struct Downloader {
@@ -28,24 +34,36 @@ impl Downloader {
 	}
 
 	async fn get(&self, url: &str) -> Result<Response> {
-		let response = reqwest::get(url).await?;
+		let response = self.client.get(url).send().await?;
 
 		if response.status().is_success() {
 			Ok(response)
 		} else {
-			bail!("Got a \"{}\" for {url:?}", response.status());
+			bail!("got a \"{}\" for {url:?}", response.status());
 		}
 	}
 
 	pub(crate) async fn get_jar(&mut self, url: &str) -> Result<Jar> {
-		// TODO: download + cache jar
+		let downloads = Path::new("./download");
 
+		let path = Path::new(url).strip_prefix(Path::new("https://"))
+			.with_context(|| anyhow!("url doesn't start with `https://`: {url:?}"))?;
 
-		// from libs:
-		// to a file given by the
-		// part after the last slash into a libraries folder (ensuring that we don't overwrite a file)
+		let path = downloads.join(path);
 
-		Ok(Jar)
+		if !path.exists() {
+			if let Some(parent) = path.parent() {
+				fs::create_dir_all(parent)?;
+			}
+
+			let response = self.get(url).await?;
+
+			let mut src = response.bytes().await?.reader();
+			let mut dest = File::create(&path)?;
+			std::io::copy(&mut src, &mut dest)?;
+		}
+
+		Ok(Jar::new(path))
 	}
 
 	async fn versions_manifest(&mut self) -> Result<&VersionsManifest> {
@@ -63,12 +81,14 @@ impl Downloader {
 	}
 
 	async fn wanted_version_manifest(&mut self, version: &Version) -> Result<VersionManifest> {
+		let minecraft_version = version.get_minecraft_version();
+
 		let manifest = self.versions_manifest().await?;
 
 		let manifest_version = manifest
 			.versions
 			.iter()
-			.find(|it| &it.id == version);
+			.find(|it| it.id == minecraft_version);
 
 		if let Some(manifest_version) = manifest_version {
 			let url = manifest_version.url.clone();
@@ -76,20 +96,20 @@ impl Downloader {
 			let body = self.get(&url).await?.text().await?;
 
 			let version_manifest: VersionManifest = serde_json::from_str(&body)
-				.with_context(|| anyhow!("Failed to parse version manifest for version {:?} from {:?}", version, url))?;
+				.with_context(|| anyhow!("failed to parse version manifest for version {:?} from {:?}", version, url))?;
 
 			Ok(version_manifest)
 		} else {
-			bail!("No version data for Minecraft version {:?}", version);
+			bail!("no version data for Minecraft version {:?}", version);
 		}
 	}
 	pub(crate) async fn version_details(&mut self, version: &Version, environment: &Environment) -> Result<VersionDetails> {
+		let minecraft_version = version.get_minecraft_version();
+
 		let manifest = self.versions_manifest().await?;
 
-		let manifest_version = manifest
-			.versions
-			.iter()
-			.find(|it| &it.id == version);
+		let manifest_version = manifest.versions.iter()
+			.find(|it| it.id == minecraft_version);
 
 		if let Some(manifest_version) = manifest_version {
 			let url = &manifest_version.details.clone();
@@ -97,22 +117,22 @@ impl Downloader {
 			let body = self.get(url).await?.text().await?;
 
 			let version_details: VersionDetails = serde_json::from_str(&body)
-				.with_context(|| anyhow!("Failed to parse version details for version {:?} from {:?}", version, url))?;
+				.with_context(|| anyhow!("failed to parse version details for version {:?} from {:?}", version, url))?;
 
 			if version_details.shared_mappings {
 				if &Environment::Merged != environment {
-					bail!("Minecraft version {:?} is only available as merged but was requested for {:?}", version, environment);
+					bail!("minecraft version {:?} is only available as merged but was requested for {:?}", version, environment);
 				}
 			} else {
 				match environment {
 					Environment::Merged => {
-						bail!("Minecraft version {:?} cannot be merged - please select either the client or server environment!", version);
+						bail!("minecraft version {:?} cannot be merged - please select either the client or server environment!", version);
 					},
 					Environment::Client if !version_details.client => {
-						bail!("Minecraft version {:?} does not have a client jar!", version);
+						bail!("minecraft version {:?} does not have a client jar!", version);
 					},
 					Environment::Server if !version_details.server => {
-						bail!("Minecraft version {:?} does not have a server jar", version);
+						bail!("minecraft version {:?} does not have a server jar", version);
 					},
 					_ => {},
 				}
@@ -120,23 +140,28 @@ impl Downloader {
 
 			Ok(version_details)
 		} else {
-			bail!("No version details for Minecraft version {:?}", version);
+			bail!("no version details for minecraft version {:?}", version);
 		}
 	}
+	/// Downloads the feather intermediary, calamus, for a given version.
+	///
+	/// The namespaces are `official` to `intermediary` (aka `calamus`) here.
 	pub(crate) async fn calamus_v2(&mut self, version: &Version) -> Result<Mappings<2>> {
-		let url = format!("https://maven.ornithemc.net/releases/net/ornithemc/calamus-intermediary/{}/calamus-intermediary-{}-v2.jar", version.0, version.0);
+		let url = format!("https://maven.ornithemc.net/releases/net/ornithemc/calamus-intermediary/{version}/calamus-intermediary-{version}-v2.jar");
 
 		let body = self.get(&url).await?.bytes().await?;
 
-		let reader = Cursor::new(body);
-
-		let mut zip = ZipArchive::new(reader)?;
+		let mut zip = ZipArchive::new(Cursor::new(body))?;
 
 		let mappings = zip.by_name("mappings/mappings.tiny")
-			.with_context(|| anyhow!("Cannot find mappings in zip file from {:?}", url))?;
+			.with_context(|| anyhow!("cannot find mappings in zip file from {:?}", url))?;
 
-		crate::reader::tiny_v2::read(mappings)
-			.with_context(|| anyhow!("Failed to read mappings from mappings/mappings.tiny of {:?}", url))
+		let mappings = crate::reader::tiny_v2::read(mappings)
+			.with_context(|| anyhow!("failed to read mappings from mappings/mappings.tiny of {:?}", url))?;
+
+		mappings.info.namespaces.check_that(["official", "intermediary"])?;
+
+		Ok(mappings)
 	}
 	pub(crate) async fn mc_libs(&mut self, version: &Version) -> Result<Vec<Jar>> {
 		let version_file = self.wanted_version_manifest(version).await?;
@@ -145,14 +170,19 @@ impl Downloader {
 
 		for lib in version_file.libraries {
 			if let Some(artifact) = lib.downloads.artifact {
-				let url = &artifact.url;
-
-				let jar = self.get_jar(&url).await?;
+				let jar = self.get_jar(&artifact.url).await?;
 
 				libs.push(jar);
 			}
 		}
 
 		Ok(libs)
+	}
+
+	pub(crate) async fn get_maven_metadata_xml(&mut self, url: &str) -> Result<MavenMetadata> {
+		let body = self.get(url).await?.text().await?;
+
+		serde_xml_rs::from_str(&body)
+			.with_context(|| anyhow!("failed to parse maven metadata xml file from {url:?}"))
 	}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
-use std::io::Cursor;
+use std::fmt::{Debug, Display, Formatter};
+use std::fs::File;
 use std::path::Path;
-use anyhow::Result;
+use std::time::Instant;
+use anyhow::{anyhow, bail, Context, Result};
+use class_file::tree::method::ParameterName;
 use crate::download::Downloader;
+use crate::download::versions_manifest::MinecraftVersion;
+use crate::jar::Jar;
 use crate::tree::mappings::Mappings;
-use crate::version_graph::{Version, VersionGraph};
+use crate::tree::names::Names;
+use crate::version_graph::VersionGraph;
 
 mod reader;
 mod version_graph;
@@ -12,8 +18,40 @@ mod download;
 mod tree;
 mod specialized_methods;
 
-#[derive(Debug)]
-struct Jar;
+mod jar;
+
+#[derive(Debug, Clone, PartialEq, Hash, Eq)]
+/// The version id used in the mappings diffs and mappings files.
+/// This can end in `-client` and `-server`, or not have any suffix at all.
+pub(crate) struct Version(String);
+
+impl Display for Version {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&self.0)
+    }
+}
+
+impl Version {
+    fn get_environment(&self) -> Environment {
+        if self.0.ends_with("-client") {
+            Environment::Client
+        } else if self.0.ends_with("-server") {
+            Environment::Server
+        } else {
+            Environment::Merged
+        }
+    }
+
+    fn get_minecraft_version(&self) -> MinecraftVersion {
+        if let Some(without) = self.0.strip_suffix("-client") {
+            MinecraftVersion(without.to_owned())
+        } else if let Some(without) = self.0.strip_suffix("-server") {
+            MinecraftVersion(without.to_owned())
+        } else {
+            MinecraftVersion(self.0.to_owned())
+        }
+    }
+}
 
 #[derive(Debug, PartialEq)]
 enum Environment {
@@ -22,180 +60,190 @@ enum Environment {
     Server,
 }
 
-impl Environment {
-    fn is_merged(&self) -> bool {
-        matches!(self, Self::Merged)
-    }
-    fn is_client(&self) -> bool {
-        matches!(self, Environment::Merged) || matches!(self, Environment::Client)
-    }
-    fn is_server(&self) -> bool {
-        matches!(self, Environment::Merged) || matches!(self, Environment::Server)
-    }
+fn inspect<const N: usize>(mappings: &Mappings<N>, path: &str) -> Result<()> {
 
-    fn parse(id: &Version) -> Environment {
-        if id.0.ends_with("-client") {
-            return Environment::Client;
-        }
-        if id.0.ends_with("-server") {
-            return Environment::Server;
-        }
-        Environment::Merged
-    }
+    // fix the order of the other file being looked at, which makes diffing easier...
+    /*
+	let mut file = File::open("/tmp/original.tiny")?;
+	let mut file_out = File::create("/tmp/original_sorted_rs.tiny")?;
+	let m: Mappings<3> = reader::tiny_v2::read(&mut file)?;
+	writer::tiny_v2::write(&m, &mut file_out)?;
+	 */
 
-    fn parse_version(&self, id: &str) -> String {
-        match self {
-            Environment::Merged => id.to_owned(),
-            Environment::Client => todo!("remove the `-client` suffix"),
-            Environment::Server => todo!("remove the `-server` suffix"),
+    let mut file = File::create(path)?;
+    writer::tiny_v2::write(mappings, &mut file)?;
+    Ok(())
+}
+
+async fn build(downloader: &mut Downloader, version_graph: &VersionGraph, version: &Version) -> Result<BuildResult> {
+
+    let feather_version = next_feather_version(downloader, version, false).await?;
+
+    let mappings = version_graph.apply_diffs(version)? // calamus -> named
+        .remove_dummy("named")?;
+
+    let main_jar = main_jar(downloader, version).await?;
+    let calamus_v2 = downloader.calamus_v2(version).await?;
+    let libraries = downloader.mc_libs(version).await?;
+
+    let build_feather_tiny = Jar::add_specialized_methods_to_mappings(&main_jar, &calamus_v2, &libraries, &mappings)
+        .context("failed to add specialized methods to mappings")?;
+
+    let merge_v2 = merge_v2(&build_feather_tiny, &calamus_v2)?;
+
+    inspect(&merge_v2, "/tmp/out.tiny")?;
+
+    let name = format!("feather-{feather_version}-mergedv2.jar");
+    let data = writer::tiny_v2::write_zip_file(&merge_v2)?;
+    let merged_feather = Jar::new_mem(name, data);
+
+    let name = format!("feather-{feather_version}-v2.jar");
+    let data = writer::tiny_v2::write_zip_file(&build_feather_tiny)?;
+    let unmerged_feather = Jar::new_mem(name, data);
+
+    Ok(BuildResult { merged_feather, unmerged_feather })
+}
+
+/// Merges the calamus intermediary with the created mappings.
+///
+/// The namespaces are `official` to `intermediary` and `named` here.
+fn merge_v2(feather: &Mappings<2>, calamus: &Mappings<2>) -> Result<Mappings<3>> {
+    let mappings_a = feather.clone().rename_namespaces(["calamus", "named"], ["intermediary", "named"])?;
+    let mappings_b = calamus.clone().reorder(["intermediary", "official"])?;
+
+    let merged = Mappings::merge(&mappings_b, &mappings_a)?
+        .apply_our_fix()?;
+
+    merged.reorder(["official", "intermediary", "named"])
+}
+
+impl Mappings<3> {
+    fn apply_our_fix(mut self) -> Result<Self> {
+        let official = self.get_namespace("official")?;
+        let intermediary = self.get_namespace("intermediary")?;
+        let named = self.get_namespace("named")?;
+
+        fn check_names<T: Debug>(names: &Names<3, T>) -> Result<()> {
+            let names: &[Option<T>; 3] = names.into();
+
+            for n in names {
+                if n.is_none() {
+                    bail!("at least one `None` in {names:?}");
+                }
+            }
+
+            // TODO: if there's a None in names, fail
+            Ok(())
         }
+
+        for c in self.classes.values_mut() {
+            if c.info.names[named].is_none() {
+                c.info.names[named] = c.info.names[intermediary].clone();
+            }
+
+            /*
+
+            // TODO: consider deref / .as_str() method..
+            let names: [Option<ClassName>; N] = names.into();
+            let names: [Option<String>; N] = names.map(|x| x.map(|x| x.into()));
+
+            if names.len() >= 3 && names[1].as_ref().is_some_and(|x| x.contains('$')) {
+                let a = names[1].as_ref().unwrap().chars().filter(|x| *x == '$').count();
+                let b = names[2].as_ref().map(|y| y.chars().filter(|x| *x == '$').count()).unwrap_or(0);
+
+                if a != b {
+                    let wrong_inner_class_name = names[2].as_deref().unwrap_or("");
+                    dbg!(wrong_inner_class_name);
+                }
+            }
+
+            let names: [Option<ClassName>; N] = names.map(|x| x.map(|x| x.into()));
+            let names = names.into();
+            */
+
+
+            check_names(&c.info.names)?;
+
+            for f in c.fields.values_mut() {
+                if c.info.names[named].is_none() {
+                    c.info.names[named] = c.info.names[intermediary].clone();
+                }
+
+                if f.info.names[named].is_none() {
+                    f.info.names[named] = f.info.names[intermediary].clone();
+                }
+
+                check_names(&f.info.names)?;
+            }
+
+            for m in c.methods.values_mut() {
+                if m.info.names[named].is_none() {
+                    m.info.names[named] = m.info.names[intermediary].clone();
+                }
+
+                if m.info.names[official].is_none() && m.info.names[intermediary].as_ref().is_some_and(|x| x == "<init>") {
+                    m.info.names[official] = m.info.names[intermediary].clone();
+                }
+
+                if m.info.names[official].is_none() && m.info.names[intermediary].as_ref().is_some_and(|x| !x.as_str().starts_with("m_")) {
+                    m.info.names[official] = m.info.names[intermediary].clone();
+                }
+
+                if m.info.names[official].is_none() {
+                    m.info.names[official] = m.info.names[intermediary].clone();
+                    //TODO: this fixes the issues where you have
+                    // c  net/minecraft/server/MinecraftServer  net/minecraft/server/MinecraftServer  net/minecaft/server/MinecraftServer
+                    //   m  ()Z  m_5733001  m_5733001  isSnooperEnabled
+                    // c  um  net/minecraft/unmapped/C_3978417  net/minecraft/snooper/SnooperPopulator
+                    //   m  ()Z  Z  m_5733001  isSnooperEnabled
+                    // which ofc is incorrect
+                    // further: make this something for the sus module
+
+                    //println!("no names: {:?}", m.info);
+                    //continue;
+                }
+
+                check_names(&m.info.names)?;
+
+                for p in m.parameters.values_mut() {
+                    if p.info.names[official].is_none() {
+                        p.info.names[official] = Some(ParameterName::from(""));
+                    }
+
+                    check_names(&p.info.names)?;
+                }
+            }
+        }
+
+        Ok(self)
+    }
+}
+
+/// Gets the jar from mojang. If it's a merged environment ([Environment::Merged]), then
+/// the two jars (client and server) will be merged.
+///
+/// This jar is in the `official` mappings, i.e. obfuscated.
+async fn main_jar(downloader: &mut Downloader, version: &Version) -> Result<Jar> {
+    let environment = version.get_environment();
+
+    let version_details = downloader.version_details(version, &environment).await?;
+
+    match environment {
+        Environment::Merged => {
+            let client = downloader.get_jar(&version_details.downloads.client.url).await?;
+            let server = downloader.get_jar(&version_details.downloads.server.url).await?;
+
+            Jar::merge(client, server).with_context(|| anyhow!("failed to merge jars for version {version}"))
+        },
+        Environment::Client => downloader.get_jar(&version_details.downloads.client.url).await,
+        Environment::Server => downloader.get_jar(&version_details.downloads.server.url).await,
     }
 }
 
 #[derive(Debug)]
-struct Build {
-    version: Version,
-    mappings: Mappings<2>,
-}
-
-impl Build {
-    fn create(version_graph: &VersionGraph, version: &Version) -> Result<Build> {
-        let mut mappings = version_graph.apply_diffs(version)?;
-
-        mappings.remove_dummy("named")?;
-
-        Ok(Build {
-            version: version.clone(),
-            mappings,
-        })
-    }
-
-    async fn build_feather_tiny(&self, downloader: &mut Downloader) -> Result<Mappings<2>> {
-        let main_jar = self.main_jar(downloader).await?;
-        let calamus_v2 = downloader.calamus_v2(&self.version).await?;
-        let libraries = downloader.mc_libs(&self.version).await?;
-        /*
-    // old:
-        let remapper = TinyRemapper(calamus_v2, "official", "intermediary")
-
-        let calamusJar = ...;
-
-        let outputConsumer = OutputConsumer(calamusJar)
-        outputConsumer.addNonClassFiles(mainJar)
-        remapper.readInputs(mainJar)
-
-        libraries.eachFileRecurse(FileType.FILES) { file ->
-            remapper.readClassPath(file.toPath())
-        }
-        remapper.apply(outputConsumer)
-        outputConsumer.close()
-        remapper.finish()
-
-        new MapSpecializedMethodsCommand().run([calamusJar, "tinyv2", self.mappings, "tinyv2:intermediary:named", _return_this_])
-
-    // new: */
-        let specialized_methods = main_jar.get_specialized_methods()?;
-        /*
-        let remapper = calamus_v2.remapper(from: "official", to: "intermediary")
-        let specialized_methods = remapper.apply(specialized_methods)
-        self.mappings.add(specialized_methods)
-		*/
-
-
-        // TODO: impl
-        Ok(self.mappings.clone())
-    }
-
-    async fn v2_unmerged_feather_jar(&self, downloader: &mut Downloader) -> Result<Jar> {
-        let mappings = self.build_feather_tiny(downloader).await?;
-
-        // TODO: put this in a "jar" somehow
-
-        let mut buf = Vec::new();
-
-        writer::tiny_v2::write_zip_file(&mappings, &mut Cursor::new(&mut buf))?;
-
-        let feather_version = "0.0.0";
-        let file_name = format!("feather-{feather_version}-v2.jar");
-
-        Ok(Jar)
-    }
-
-    async fn invert_calamus_v2(&self, downloader: &mut Downloader) -> Result<Mappings<2>> {
-        downloader.calamus_v2(&self.version)
-            .await?
-            .reorder(["intermediary", "official"])
-    }
-
-    async fn merge_v2(&self, downloader: &mut Downloader) -> Result<Mappings<3>> {
-        let mut mappings_a = self.build_feather_tiny(downloader).await?;
-        let mappings_b = self.invert_calamus_v2(downloader).await?;
-
-        mappings_a.rename_namespaces(["calamus", "named"], ["intermediary", "named"])?;
-
-        let merged = Mappings::merge(&mappings_b, &mappings_a)?;
-
-        let output = merged.reorder(["official", "intermediary", "named"])?;
-
-        Ok(output)
-    }
-
-    async fn v2_merged_feather_jar(&self, downloader: &mut Downloader) -> Result<Jar> {
-        let merge_v2 = self.merge_v2(downloader).await?;
-
-        // TODO: put this in a "jar" somehow
-
-        let mut buf = Vec::new();
-
-        writer::tiny_v2::write_zip_file(&merge_v2, &mut Cursor::new(&mut buf))?;
-
-        let feather_version = "0.0.0";
-        let file_name = format!("feather-{feather_version}-mergedv2.jar"); // TODO: ask space if that missing - is wanted: merged-v2
-
-        Ok(Jar)
-    }
-
-    async fn build(&self, downloader: &mut Downloader) -> Result<()> {
-        self.v2_merged_feather_jar(downloader).await?;
-        self.v2_unmerged_feather_jar(downloader).await?;
-        // TODO: impl: be done with the results from both calls above!
-        Ok(())
-    }
-
-    async fn main_jar(&self, downloader: &mut Downloader) -> Result<Jar> {
-        let environment = Environment::parse(&self.version);
-
-        let version_details = downloader.version_details(&self.version, &environment).await?;
-
-        match environment {
-            Environment::Merged => {
-                let client = downloader.get_jar(&version_details.downloads.client.url).await?;
-                let server = downloader.get_jar(&version_details.downloads.server.url).await?;
-
-                // TODO: merge the jars
-                // like this:
-                //   def jarMerger = new JarMerger(clientJar, serverJar, mergedJar)
-                //   jarMerger.merge()
-                //   jarMerger.close()
-                // and return mergedJar
-                // note that clientJar and serverJar are set in downloadMcJars
-
-                let jar = Jar; // merge here
-
-                Ok(jar)
-            },
-            Environment::Client => {
-                let url = &version_details.downloads.client.url;
-
-                downloader.get_jar(&url).await
-            },
-            Environment::Server => {
-                let url = &version_details.downloads.server.url;
-
-                downloader.get_jar(&url).await
-            },
-        }
-    }
+struct BuildResult {
+    merged_feather: Jar,
+    unmerged_feather: Jar,
 }
 
 #[tokio::main]
@@ -213,8 +261,43 @@ async fn main() -> Result<()> {
 
     let version = v.get("1.12.2").unwrap();
 
-    let build = Build::create(&v, version)?;
-    build.build(&mut downloader).await?;
+    let start = Instant::now();
+
+    let result = build(&mut downloader, &v, version).await?;
+
+    println!("building took {:?}", start.elapsed());
+
+	dbg!(result.merged_feather);
+	dbg!(result.unmerged_feather);
 
     Ok(())
+}
+
+async fn next_feather_version(downloader: &mut Downloader, version: &Version, local: bool) -> Result<String> {
+    if local {
+        Ok(format!("{version}+build.local"))
+    } else {
+        let url = "https://maven.ornithemc.net/releases/net/ornithemc/feather/maven-metadata.xml";
+
+        let mut build_number = 0;
+
+        // Note: we consider it a hard failure if the maven-metadata.xml file does no exist.
+        // However if you don't have this file yet, you can comment out the lines below to start at build number 1.
+        let metadata = downloader.get_maven_metadata_xml(url).await?;
+
+        let version_build = format!("{version}");
+
+        for version in metadata.versioning.versions.versions {
+            if let Some((left, right)) = version.split_once("+build.") {
+                if left == version_build {
+                    let number = right.parse()?;
+                    build_number = build_number.max(number);
+                }
+            }
+        }
+
+        let next_build_number = build_number + 1;
+
+        Ok(format!("{version}+build.{next_build_number}"))
+    }
 }
