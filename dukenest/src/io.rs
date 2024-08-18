daadc7fb48d50b075afd2406f3758788ff985772
zeichenreihe <67289119+zeichenreihe@users.noreply.github.com>
2024-08-18 19:07:16 +0200

    add `dukenest`
    this is all code related to nesting things

---
+++ dukenest/src/io.rs
@@ -0,0 +1,86 @@
+use std::io::{BufRead, BufReader, Read};
+use anyhow::{anyhow, bail, Context, Result};
+use duke::tree::class::InnerClassFlags;
+use duke::tree::method::MethodNameAndDesc;
+use crate::{Nest, Nests, NestType};
+
+impl Nests {
+	pub fn read(vec: &Vec<u8>) -> Result<Nests> {
+		let mut slice = vec.as_slice();
+		Self::read_from_reader(&mut slice)
+	}
+
+	fn read_from_reader(reader: impl Read) -> Result<Nests> {
+		let mut nests = Nests::new();
+
+		for (line_number, line) in BufReader::new(reader).lines().enumerate() {
+			let line_number = line_number + 1;
+			let line = line?;
+
+			let vec: Vec<&str> = line.split('\t').collect();
+			if vec.len() != 6 {
+				bail!("invalid mapping {line:?} in {line_number}: wrong number of fields: {}, expected 6", vec.len());
+			}
+			let array: [&str; 6] = vec.try_into().unwrap(); // can't panic, we checked the size
+
+			let [class_name, encl_class_name, encl_method_name,
+				encl_method_desc, inner_name, access_string] = array;
+
+			if class_name.is_empty() {
+				bail!("invalid mapping {line:?} in line {line_number}: missing class name argument");
+			}
+			if encl_class_name.is_empty() {
+				bail!("invalid mapping {line:?} in line {line_number}: missing enclosing class name argument");
+			}
+			if inner_name.is_empty() {
+				bail!("invalid mapping {line:?} in line {line_number}: missing inner class name argument");
+			}
+
+			let encl_method = if encl_method_name.is_empty() || encl_method_desc.is_empty() {
+				None
+			} else {
+				Some(MethodNameAndDesc {
+					name: encl_method_name.to_owned().into(),
+					desc: encl_method_desc.to_owned().into(),
+				})
+			};
+
+			let access = || -> Result<_> { Ok( if let Some(hex_access) = access_string.strip_prefix("0x") {
+				u16::from_str_radix(hex_access, 16)?
+			} else if let Some(binary_access) = access_string.strip_prefix("0b") {
+				u16::from_str_radix(binary_access, 2)?
+			} else {
+				access_string.parse()?
+			} ) };
+			let access = access().with_context(|| anyhow!("invalid mapping {line:?} in line {line_number}: invalid access flags"))?;
+
+			// TODO: dangerous string position stuff?
+			let idx = inner_name.char_indices()
+				.take_while(|(_, ch)| ch.is_ascii_digit())
+				.map(|(pos, _)| pos)
+				.last()
+				.unwrap_or(0);
+
+			let nest_type = if idx == inner_name.len() { // TODO: this test might be wrong...
+				NestType::Anonymous
+			} else if idx == 0 {
+				NestType::Inner
+			} else {
+				NestType::Local
+			};
+
+			let nest = Nest {
+				nest_type,
+				class_name: class_name.to_owned().into(),
+				encl_class_name: encl_class_name.to_owned().into(),
+				encl_method,
+				inner_name: inner_name.to_owned(),
+				inner_access: InnerClassFlags::from(access),
+			};
+
+			nests.add(nest);
+		}
+
+		Ok(nests)
+	}
+}
---
+++ dukenest/src/lib.rs
@@ -0,0 +1,372 @@
+use std::collections::HashSet;
+use anyhow::Result;
+use indexmap::{IndexMap, IndexSet};
+use duke::tree::class::{ClassAccess, ClassFile, ClassName, EnclosingMethod, InnerClass, InnerClassFlags};
+use duke::tree::method::MethodNameAndDesc;
+use dukebox::{BasicFileAttributes, Jar, JarEntry, OpenedJar};
+use dukebox::parsed::{ParsedJar, ParsedJarEntry};
+use quill::remapper::{ARemapper, ARemapperAsBRemapper, BRemapper, NoSuperClassProvider};
+use quill::tree::mappings::Mappings;
+
+mod io;
+
+#[derive(Clone)]
+pub(crate) enum NestType {
+	Anonymous,
+	Inner,
+	Local,
+}
+
+pub(crate) struct Nest {
+	pub(crate) nest_type: NestType,
+
+	pub(crate) class_name: ClassName,
+	pub(crate) encl_class_name: ClassName,
+	pub(crate) encl_method: Option<MethodNameAndDesc>,
+
+	pub(crate) inner_name: String,
+	pub(crate) inner_access: InnerClassFlags,
+}
+
+pub struct Nests {
+	pub(crate) all: IndexMap<ClassName, Nest>,
+}
+
+impl Nests {
+	pub(crate) fn new() -> Nests {
+		Nests { all: IndexMap::new() }
+	}
+
+	pub(crate) fn add(&mut self, nest: Nest) {
+		self.all.insert(nest.class_name.clone(), nest);
+	}
+}
+
+pub struct NesterOptions {
+	silent: bool,
+	remap: bool,
+}
+
+impl Default for NesterOptions {
+	fn default() -> Self {
+		NesterOptions { silent: false, remap: true }
+	}
+}
+
+impl NesterOptions {
+	pub fn silent(self, silent: bool) -> NesterOptions {
+		NesterOptions { silent, ..self }
+	}
+
+	pub fn remap(self, remap: bool) -> NesterOptions {
+		NesterOptions { remap, ..self }
+	}
+}
+
+// we assume class_node.name matches the name of the JarEntry
+
+pub fn nest_jar(options: NesterOptions, src: &impl Jar, nests: Nests) -> Result<ParsedJar> {
+	let mut class_version = None;
+	let mut jar_new_classes = IndexMap::new();
+	let mut methods_map: IndexMap<ClassName, HashSet<MethodNameAndDesc>> = IndexMap::new();
+	let mut classes_in_jar: IndexSet<ClassName> = IndexSet::new();
+
+	let mut opened_src = src.open()?;
+
+	for key in opened_src.entry_keys() {
+		let entry = opened_src.by_entry_key(key)?;
+
+		// TODO: the original code reads this with a reader that SKIP_{FRAMES,CODE,DEBUG}
+		//  my guess is that we could do even better by providing our own ClassVisitor impl here...
+		if let ParsedJarEntry::Class { class, .. } = entry.to_parsed_jar_entry()? {
+			let class_node = class.read()?;
+
+			if class_version.is_none() || class_version.is_some_and(|class_version| class_node.version < class_version) {
+				class_version = Some(class_node.version);
+			}
+
+			let methods = class_node.methods.into_iter()
+				.map(|m| MethodNameAndDesc {
+					name: m.name,
+					desc: m.descriptor,
+				})
+				.collect();
+
+			classes_in_jar.insert(class_node.name.clone());
+			methods_map.insert(class_node.name, methods);
+		}
+	}
+
+	let methods_map = methods_map;
+
+	let class_version = class_version.unwrap();
+
+	let this_nests: IndexMap<_, _> = nests.all
+		.into_iter()
+		.filter(|(_, nest)| {
+			// _ is nest.class_name!
+
+			if !classes_in_jar.contains(&nest.class_name) {
+				return false;
+			}
+
+			if !classes_in_jar.contains(&nest.encl_class_name) {
+				jar_new_classes.entry(nest.encl_class_name.clone())
+					.or_insert_with(|| ClassFile::new(
+						class_version,
+						ClassAccess {
+							is_public: true,
+							..ClassAccess::default()
+						},
+						nest.encl_class_name.clone(),
+						Some(ClassName::JAVA_LANG_OBJECT.clone()),
+						vec![]
+					));
+
+				classes_in_jar.insert(nest.encl_class_name.clone());
+			}
+
+			let has_encl_method = nest.encl_method.as_ref().and_then(|encl_method| {
+				methods_map.get(&nest.encl_class_name)
+					.map(|methods| methods.contains(encl_method))
+			})
+				.unwrap_or_default();
+
+			match nest.nest_type {
+				// anonymous class may have an enclosing method, they may not
+				// for anonymous classes, the inner name is typically a number: their anonymous class index
+				NestType::Anonymous => nest.inner_name.parse::<i32>().map_or(false, |x| x >= 1),
+
+				// inner classes NEVER have an enclosing method
+				NestType::Inner => !has_encl_method,
+
+				// local classes ALWAYS have an enclosing method
+				NestType::Local => has_encl_method,
+			}
+		})
+		.collect();
+
+	if !options.silent {
+		println!("Prepared {} nests...", this_nests.len());
+	}
+
+	let mut dst_resulting_entries = IndexMap::new();
+
+	// only when remapping it's needed
+		fn remap(this_nests: &IndexMap<ClassName, Nest>, corresponding_nest: &Nest) -> ClassName {
+			let mut result = this_nests.get(&corresponding_nest.encl_class_name)
+				.map(|nest| remap(this_nests, nest))
+				.unwrap_or_else(|| corresponding_nest.encl_class_name.clone());
+
+			let s = result.as_mut_string();
+			s.push('$');
+			s.push_str(corresponding_nest.inner_name.as_str());
+
+			result
+		}
+
+		let map = this_nests.iter()
+			.map(|(old_name, nest)| (old_name, remap(&this_nests, nest)))
+			.filter(|(old_name, new_name)| &new_name != old_name)
+			.collect();
+
+		struct MyRemapper<'a>(IndexMap<&'a ClassName, ClassName>);
+		impl ARemapper for MyRemapper<'_> {
+			fn map_class_fail(&self, class: &ClassName) -> Result<Option<ClassName>> {
+				Ok(self.0.get(class).cloned())
+			}
+		}
+
+		let remapper = ARemapperAsBRemapper(MyRemapper(map));
+	// end of that
+
+	for new_class in jar_new_classes.into_values() {
+		let name = new_class.name.as_str().to_owned() + ".class";
+
+		let class_node = do_nested_class_attribute_class_visitor(&this_nests, new_class);
+
+		let entry_attr = BasicFileAttributes::new_empty();
+		let entry = ParsedJarEntry::Class { attr: entry_attr, class: class_node.into() };
+
+
+		if options.remap {
+			let name = dukebox::remap::remap_jar_entry_name(name, &remapper)?;
+			let entry = dukebox::remap::remap_jar_entry(entry, &remapper)?;
+
+			dst_resulting_entries.insert(name, entry);
+		} else {
+			dst_resulting_entries.insert(name, entry);
+		}
+	}
+
+
+	for key in opened_src.entry_keys() {
+		let entry = opened_src.by_entry_key(key)?;
+
+		let name = entry.name().to_owned();
+
+		match entry.to_parsed_jar_entry()? {
+			ParsedJarEntry::Class { attr, class } => {
+				let class_node = class.read()?;
+
+				let class_node = do_nested_class_attribute_class_visitor(&this_nests, class_node);
+
+				let entry = ParsedJarEntry::Class { attr, class: class_node.into() };
+
+				if options.remap {
+					let name = dukebox::remap::remap_jar_entry_name(name, &remapper)?;
+					let entry = dukebox::remap::remap_jar_entry(entry, &remapper)?;
+
+					dst_resulting_entries.insert(name, entry);
+				} else {
+					dst_resulting_entries.insert(name, entry);
+				}
+			},
+			entry => {
+				dst_resulting_entries.insert(name, entry);
+			},
+		}
+	}
+
+	// TODO: use log::_? also do we really need this?
+	if !options.silent {
+		println!("Applied nests...");
+		if options.remap {
+			println!("Remapped nested classes...");
+		}
+		println!("Moved over non-class files...");
+		println!("Sorted class files...");
+		println!("Done!");
+	}
+
+	Ok(ParsedJar { entries: dst_resulting_entries })
+
+}
+
+fn do_nested_class_attribute_class_visitor(this_nests: &IndexMap<ClassName, Nest>, mut class_node: ClassFile) -> ClassFile {
+
+	if let Some(nest) = this_nests.get(&class_node.name) {
+		if matches!(nest.nest_type, NestType::Anonymous | NestType::Local) {
+			class_node.enclosing_method = Some(EnclosingMethod {
+				class: nest.encl_class_name.clone(),
+				method: nest.encl_method.clone(),
+			});
+		}
+
+		class_node.inner_classes.get_or_insert_with(Vec::new)
+			.push(InnerClass {
+				inner_class: nest.class_name.clone(),
+				outer_class: if matches!(nest.nest_type, NestType::Inner) {
+					Some(nest.encl_class_name.clone())
+				} else {
+					None
+				},
+				inner_name: if matches!(nest.nest_type, NestType::Inner | NestType::Local) {
+					Some(strip_local_class_prefix(&nest.inner_name))
+				} else {
+					None
+				},
+				flags: nest.inner_access,
+			});
+	}
+
+	class_node
+}
+
+fn strip_local_class_prefix(inner_name: &str) -> String {
+	// local class names start with a number prefix
+	let idx = inner_name.char_indices()
+		.take_while(|(_, ch)| ch.is_ascii_digit())
+		.map(|(pos, _)| pos)
+		.last()
+		.unwrap_or(0);
+
+	// if entire inner name is a number, this class is anonymous, not local
+	if idx == inner_name.len() {
+		inner_name.to_owned()
+	} else {
+		// TODO: dangerous direct access to string slice...
+		inner_name[idx..].to_owned()
+	}
+}
+
+
+pub fn map_nests(mappings: &Mappings<2>, nests: Nests) -> Result<Nests> {
+	let remapper = mappings.remapper_b_first_to_second(NoSuperClassProvider::new())?;
+
+	let mut dst = Nests::new();
+
+	for (_, nest) in nests.all {
+
+		let mapped_name = remapper.map_class(&nest.class_name)?;
+
+		let (encl_class_name, inner_name) = if let Some((encl_class_name, inner_name)) = mapped_name.as_str().rsplit_once("__") {
+			// provided mappings already use nesting
+			(encl_class_name.to_owned().into(), inner_name.to_owned())
+		} else {
+			let encl_class_name = remapper.map_class(&nest.encl_class_name)?;
+
+			let inner_name = {
+				let i = nest.inner_name.char_indices()
+					.take_while(|(_, ch)| ch.is_ascii_digit())
+					.map(|(pos, _)| pos)
+					.last()
+					.unwrap_or(0);
+
+				if i < nest.inner_name.len() {
+					// TODO: dangerous direct access to string slice...
+					// local classes have a number prefix
+					let prefix = &nest.inner_name[0..i];
+					let simple_name = &nest.inner_name[i..];
+
+					// make sure the class does not have custom inner name
+					if nest.class_name.as_str().ends_with(simple_name) {
+						let mut s = String::new();
+						s.push_str(prefix);
+						s.push_str(if let Some((_, substring)) = mapped_name.as_str().rsplit_once('/') {
+							substring
+						} else {
+							mapped_name.as_str()
+						});
+						s
+					} else {
+						nest.inner_name
+					}
+				} else {
+					// anonymous class
+					let simple_name = if let Some((_, substring)) = mapped_name.as_str().rsplit_once('/') {
+						substring
+					} else {
+						mapped_name.as_str()
+					};
+
+					if let Some(number) = simple_name.strip_prefix("C_") {
+						// mapped name is Calamus intermediary format C_<number>
+						// we strip the C_ prefix and keep the number as the inner name
+						number.to_owned()
+					} else {
+						// keep the inner name given by the nests file
+						nest.inner_name
+					}
+				}
+			};
+
+			(encl_class_name, inner_name)
+		};
+
+		let nest = Nest {
+			nest_type: nest.nest_type,
+			class_name: mapped_name,
+			encl_class_name,
+			encl_method: nest.encl_method.map(|method_name_and_desc| {
+				remapper.map_method_name_and_desc(&nest.encl_class_name, &method_name_and_desc)
+			}).transpose()?,
+			inner_name,
+			inner_access: nest.inner_access,
+		};
+
+		dst.add(nest);
+	}
+
+	Ok(dst)
+}
