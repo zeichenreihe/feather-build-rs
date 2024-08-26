9c5adaa880e7c1da9be35312a2b9c53805e6049b
zeichenreihe <67289119+zeichenreihe@users.noreply.github.com>
2024-08-26 14:48:22 +0200

    quill: enigma dir: improve reading code, add missing constant from previous commit

--- quill/src/enigma_dir.rs
+++ quill/src/enigma_dir.rs
@@ -14,6 +14,8 @@ use crate::tree::mappings::{MappingInfo, Mappings};
 use crate::tree::names::Namespaces;
 use crate::tree::NodeInfo;
 
+const MAPPING_EXTENSION: &str = "mapping";
+
 pub fn read(path: impl AsRef<Path>, namespaces: Namespaces<2>) -> Result<Mappings<2>> {
 	fn walk_dir(dir: &Path) -> Result<Vec<PathBuf>> {
 		let mut paths = Vec::new();
@@ -34,24 +36,18 @@ pub fn read(path: impl AsRef<Path>, namespaces: Namespaces<2>) -> Result<Mapping
 		Ok(paths)
 	}
 
-	let paths_1 = walk_dir(path.as_ref())
-		.with_context(|| anyhow!("failed to read mappings dir {:?}", path.as_ref()))?;
-
-	let mut paths_2 = Vec::new();
-	for path in paths_1 {
-		let file_name = path.file_name().and_then(|x| x.to_str())
-			.with_context(|| anyhow!("can't get file name as string: {path:?}"))?;
+	let mut paths: Vec<_> = walk_dir(path.as_ref())
+		.with_context(|| anyhow!("failed to read mappings dir {:?}", path.as_ref()))?
+		.into_iter()
 		// skip non enigma mapping files
-		if file_name.ends_with(".mapping") {
-			paths_2.push(path);
-		}
-	}
+		.filter(|path| path.extension().is_some_and(|ex| ex == MAPPING_EXTENSION))
+		.collect();
 
 	// make it deterministic
-	paths_2.sort();
+	paths.sort();
 
 	let mut mappings = Mappings::new(MappingInfo { namespaces });
-	for path in paths_2 {
+	for path in paths {
 		crate::enigma_file::read_file_into(path, &mut mappings)?;
 	}
 
