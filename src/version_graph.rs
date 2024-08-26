e9b76d1cf323e901bf5f8c2ca3689280236153c4
zeichenreihe <67289119+zeichenreihe@users.noreply.github.com>
2024-08-26 02:16:39 +0200

    feather-build-rs: add version shortcuts for versions with ugly ids

--- src/version_graph.rs
+++ src/version_graph.rs
@@ -52,6 +52,71 @@ pub(crate) enum Environment {
 	Server,
 }
 
+const VERSION_SHORTCUTS: [(&str, &str); 53] = [
+	("a1.0.5", "a1.0.5-2149-client"),
+	("a1.0.13_01", "a1.0.13_01-1444-client"),
+	("a1.0.14", "a1.0.14-1659-client"),
+	("a1.1.0", "a1.1.0-131933-client"),
+	("a1.2.3_01", "a1.2.3_01-0958-client"),
+	("a0.2.5-server", "server-a0.2.5-1004-server"),
+	("server-a0.2.5", "server-a0.2.5-1004-server"),
+	("b1.1-client", "b1.1-1255-client"),
+	("b1.1-server", "b1.1-1245-server"),
+	("b1.3-client", "b1.3-1750-client"),
+	("b1.3-server", "b1.3-1731-server"),
+	("b1.4-client", "b1.4-1634-client"),
+	("b1.4-server", "b1.4-1507-server"),
+	("b1.8-pre1-server", "b1.8-pre1-201109091357-server"),
+	("b1.9-pre3-client", "b1.9-pre3-201110061402-client"),
+	("b1.9-pre3-server", "b1.9-pre3-201110061350-server"),
+	("b1.9-pre4-client", "b1.9-pre4-201110131434-client"),
+	("b1.9-pre4-server", "b1.9-pre4-201110131440-server"),
+	("12w05a", "12w05a-1442"),
+	("12w05a-client", "12w05a-1442-client"),
+	("12w05a-server", "12w05a-1442-server"),
+	("1.0", "1.0.0"),
+	("1.0-client", "1.0.0-client"),
+	("1.0-server", "1.0.0-server"),
+	("1.3", "1.3-pre-07261249"),
+	("1.4", "1.4-pre"),
+	("1.4.1", "1.4.1-pre-10231538"),
+	("13w03a", "13w03a-1647"),
+	("13w05a", "13w05a-1538"),
+	("13w06a", "13w06a-1636"),
+	("13w16a", "13w16a-04192037"),
+	("13w16b", "13w16b-04232151"),
+	("13w23b", "13w23b-06080101"),
+	("1.6", "1.6-pre-06251516"),
+	("1.6.2", "1.6.2-091847"),
+	("1.6.3", "1.6.3-pre-171231"),
+	("13w36a", "13w36a-09051446"),
+	("13w36b", "13w36b-09061310"),
+	("13w41b", "13w41b-1523"),
+	("1.7", "1.7-pre"),
+	("1.7.1", "1.7.1-pre"),
+	("1.7.3", "1.7.3-pre"),
+	("1.7.7", "1.7.7-101331"),
+	("14w04b", "14w04b-1554"),
+	("14w27b", "14w27b-07021646"),
+	("14w34c", "14w34c-08191549"),
+	("16w50a", "16w50a-1438"),
+	("1.12-pre3", "1.12-pre3-1409"),
+	("2point0_red", "af-2013-red"),
+	("2point0_blue", "af-2013-blue"),
+	("2point0_purple", "af-2013-purple"),
+	("15w14a", "af-2015"),
+	("1.RV-Pre1", "af-2016"),
+];
+
+fn map_shortcut(version: &str) -> &str {
+	for (shortcut, long) in VERSION_SHORTCUTS {
+		if shortcut == version {
+			return long
+		}
+	}
+	version
+}
+
 const MAPPINGS_EXTENSION: &str = ".tiny";
 const DIFF_EXTENSION: &str = ".tinydiff";
 
@@ -132,8 +197,12 @@ impl VersionGraph {
 	}
 
 	pub(crate) fn get(&self, string: &str) -> Result<&Version> {
+		let string = map_shortcut(string);
 		let version = Version(string.to_owned());
-		self.versions.get(&version).copied().map(|node| &self.graph[node]).with_context(|| anyhow!("unknown version {string:?}"))
+		self.versions.get(&version)
+			.copied()
+			.map(|node| &self.graph[node])
+			.with_context(|| anyhow!("unknown version {string:?}"))
 	}
 
 	pub(crate) fn apply_diffs(&self, target_version: &Version) -> Result<Mappings<2>> {
