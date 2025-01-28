f6e4ef9bf58449577d38a03b935ed1fe82bb5834
zeichenreihe <67289119+zeichenreihe@users.noreply.github.com>
2025-01-28 23:48:08 +0100

    quill: use `walkdir` crate for reading enigma mappings from a directory

--- quill/src/enigma_dir.rs
+++ quill/src/enigma_dir.rs
@@ -6,10 +6,10 @@
 //! # Writing
 // TODO: doc
 
-use std::collections::VecDeque;
 use std::fs::File;
-use std::path::{Path, PathBuf};
+use std::path::Path;
 use anyhow::{anyhow, bail, Context, Result};
+use walkdir::WalkDir;
 use crate::tree::mappings::{MappingInfo, Mappings};
 use crate::tree::names::Namespaces;
 use crate::tree::NodeInfo;
@@ -17,41 +17,23 @@ use crate::tree::NodeInfo;
 const MAPPING_EXTENSION: &str = "mapping";
 
 pub fn read(path: impl AsRef<Path>, namespaces: Namespaces<2>) -> Result<Mappings<2>> {
-	fn walk_dir(dir: &Path) -> Result<Vec<PathBuf>> {
-		let mut paths = Vec::new();
-
-		let mut queue: VecDeque<_> = vec![dir.to_owned()].into();
-		while let Some(dir) = queue.pop_front() {
-			for entry in std::fs::read_dir(dir)? {
-				let entry = entry?;
-				let path = entry.path();
-				if path.metadata()?.is_dir() {
-					queue.push_back(path);
-				} else {
-					paths.push(path);
-				}
-			}
-		}
-
-		Ok(paths)
-	}
-
-	let mut paths: Vec<_> = walk_dir(path.as_ref())
-		.with_context(|| anyhow!("failed to read mappings dir {:?}", path.as_ref()))?
+	WalkDir::new(path.as_ref())
+		.sort_by_file_name() // make it deterministic
 		.into_iter()
-		// skip non enigma mapping files
-		.filter(|path| path.extension().is_some_and(|ex| ex == MAPPING_EXTENSION))
-		.collect();
-
-	// make it deterministic
-	paths.sort();
-
-	let mut mappings = Mappings::new(MappingInfo { namespaces });
-	for path in paths {
-		crate::enigma_file::read_file_into(path, &mut mappings)?;
-	}
-
-	Ok(mappings)
+		.filter_entry(|entry| {
+			!entry.file_type().is_dir() &&
+				// skip non enigma mapping files
+				entry.path().extension().is_some_and(|ex| ex == MAPPING_EXTENSION)
+		})
+		.map(|res| res.map(|entry| entry.into_path()))
+		.try_fold(
+			Mappings::new(MappingInfo { namespaces }),
+			|mut mappings, path| {
+				let path = path?;
+				crate::enigma_file::read_file_into(path, &mut mappings)?;
+				Ok(mappings)
+			}
+		)
 }
 
 // TODO: doc
